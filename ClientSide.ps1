61118e13f146b8b81ee691d8577b239c2a67d109
noobElias <76161652+noobElias@users.noreply.github.com>
2022-11-11 13:40:06 +0100

    Create ClientSide.ps1

---
+++ ClientSide.ps1
@@ -0,0 +1,62 @@
+#Api for enrollment will not function unless given atleast serial and hardwarehash in json format
+#Api for pinging will not function unless given serialnumber in json format
+
+#set some params :) 
+$uri = '' # Function app URL for enrolling into autopilto
+$GroupTag = '' #optional autopilot grouptag 
+$pinguri = '' # function app URL for pinging autopilot
+
+#install "getautopiltoinfo" script to get the hardwarehash
+if(-not(test-path "C:\HWID")){
+    New-Item -Type Directory -Path "C:\HWID"
+}
+Set-Location -Path "C:\HWID"
+$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned -Force
+Install-Script -Name Get-WindowsAutopilotInfo
+
+#if $grouptag variable is defined include it 
+if($GroupTag){
+    $hwid = Get-WindowsAutopilotInfo -GroupTag $GroupTag
+}
+else{
+    $hwid = Get-WindowsAutopilotInfo
+}
+
+#convert output to json
+$hwid = $hwid | ConvertTo-Json
+
+#if prev steps are OK and the $hwid -ne $null is present send to app, else throw error
+if($hwid){
+    #the app should return output from graph showing the object that was uploaded 
+    $res = Invoke-RestMethod -Method Post -uri $uri -Body $hwid -ContentType "application/json"
+}
+else{
+    Write-Error "HWID mangler, noe er galt..."
+}
+
+#json for pinging autopilto
+$json = @{
+    serialNumber = $res.serialNumber
+    
+} | ConvertTo-Json
+
+function recurseping{
+    param($pinguri, $json)
+    $apres = Invoke-RestMethod -Method Post -Uri $pinguri -Body $json -ContentType "application/json"
+    if($apres.'@odata.count'  -eq 0){
+        Write-Host "ingen objekt, tar en blund"
+        Start-Sleep -Seconds 180
+        recurseping -pinguri $pinguri -json $json
+
+    }
+    else{
+        return $apres.value
+    }
+}
+
+#her kan man også se etter assignmentstatus. Objektet kan finnes i autopilot uten at en profil er tildelt! 
+#ping autopilot to know when object is present
+if(recurseping -pinguri $pinguri -json $json){
+    Write-Host "done and dusted som de sier" 
+}
